eeb279244eedb160f9dfee179bd82aa86ee50891
mitz <mitz@cybozu.co.jp>
2019-01-25 20:05:11 +0900

    Wait argocd-config is Healthy and guestbook is Synced

--- test/setup.go
+++ test/setup.go
@@ -167,13 +167,39 @@ func testSetup() {
 			return nil
 		}).Should(Succeed())
 
-		By("synchronizing guestbook sample app")
-		// To apply target commitID for auto-sync enabled app, use kubectl patch allows change targetRevision.
-		execSafeAt(boot0, "kubectl", "patch", "-n", argoCDNamespace, "app", "guestbook",
-			"--type='merge'", "-p", `{"spec": {"source": { "targetRevision":"`+commitID+`" } } }`)
-
+		By("checking argocd-config app status")
 		Eventually(func() error {
-			stdout, stderr, err := execAt(boot0, "argocd", "app", "sync", "guestbook")
+			stdout, stderr, err := execAt(boot0, "kubectl", "get", "app", "argocd-config", "-n", argoCDNamespace, "-o", "json")
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			var app argoappv1.Application
+			err = json.Unmarshal(stdout, &app)
+			if err != nil {
+				return err
+			}
+
+			for _, r := range app.Status.Resources {
+				if r.Status != argoappv1.SyncStatusCodeSynced {
+					return fmt.Errorf("app is not yet Synced: %s", r.Status)
+				}
+				if r.Health.Status != argoappv1.HealthStatusHealthy {
+					return fmt.Errorf("app is not yet Healthy: %s", r.Health.Status)
+				}
+			}
+			return nil
+		}).Should(Succeed())
+
+		By("synchronizing guestbook sample app")
+		Eventually(func() error {
+			// To apply target commitID for auto-sync enabled app, use kubectl patch allows change targetRevision.
+			stdout, stderr, err := execAt(boot0, "kubectl", "patch",
+				"-n", argoCDNamespace, "app", "guestbook", "--type='merge'",
+				"-p", `{"spec": {"source": { "targetRevision":"`+commitID+`" } } }`)
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			stdout, stderr, err = execAt(boot0, "argocd", "app", "sync", "guestbook", "--timeout", "20")
 			if err != nil {
 				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
 			}
