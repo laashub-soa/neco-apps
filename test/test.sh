e21804421668cd55031acd12883e16e0f4ba1538
mitz <mitz@cybozu.co.jp>
2019-01-28 13:02:57 +0900

    Load snapshot only when test directory specified

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 DIR="$1"
-
-pmctl snapshot load init
-
 PLACEMAT_PID=$(echo $(pgrep placemat) | tr " " ",")
 operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
-cd ${DIR}
+
+if [ -z "${DIR}" ]; then
+    # Load snapshot only when no DIR specified
+    pmctl snapshot load init
+else
+    cd ${DIR}
+fi
+
 sudo -E nsenter -t ${operation_pid} -n sh -c "export PATH=$PATH; $GINKGO"
 exit $?
