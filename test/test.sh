b5d86d3cb59b3154ca7fe012237d18c4f61a54fb
zoetrope <a.ikezoe@gmail.com>
2019-04-16 15:48:22 +0900

    sync with NTP after loading from snapshot

--- test/test.sh
+++ test/test.sh
@@ -24,4 +24,18 @@ done
 EXTERNAL_PID=$(pmctl pod show external | jq .pid)
 export EXTERNAL_PID
 
+# Sync with NTP. Nodes restored from snapshot will be out of time.
+echo "wait for boot servers..."
+for boot in boot-0 boot-1 boot-2 boot-3; do
+  for i in $(seq 300); do
+    if ./dcssh cybozu@${boot} date 2>/dev/null; then
+      break
+    fi
+    sleep 1
+  done
+  ./dcssh cybozu@${boot} podenter chrony chronyc makestep
+done
+
+./dcssh cybozu@boot-0 "for host in \$(sabactl machines get --role worker | jq -r '.[] | .spec.ipv4[0]'); do ckecli ssh cybozu@\${host} /opt/bin/podenter chrony chrony chronyc makestep; done"
+
 sudo -E nsenter -t $(pmctl pod show operation | jq .pid) -n sh -c "export PATH=$PATH; $GINKGO"
