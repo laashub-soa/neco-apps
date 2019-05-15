8cb9ccd5f3cc125df37bfc5f7c84be4c1555d067
mitz <mitz@cybozu.co.jp>
2019-05-15 16:17:29 +0900

    Use type public neco domain for testing

--- ingress/test/external-dns.go
+++ ingress/test/external-dns.go
@@ -37,6 +37,7 @@ func testExternalDNS() {
 	})
 
 	It("should create DNS record", func() {
+		domainName := test.TestID + ".neco-ops.dev-ne.co"
 		By("deploying DNSEndpoint")
 		dnsEndpoint := fmt.Sprintf(`
 apiVersion: externaldns.k8s.io/v1alpha1
@@ -46,91 +47,53 @@ metadata:
   namespace: test-ingress
 spec:
   endpoints:
-  - dnsName: %s.neco-ops.cybozu-ne.co
+  - dnsName: %s
     recordTTL: 180
     recordType: A
     targets:
     - 10.0.5.9
-`, test.TestID)
+`, domainName)
 		_, stderr, err := test.ExecAtWithInput(test.Boot0, []byte(dnsEndpoint), "kubectl", "apply", "-f", "-")
 		Expect(err).NotTo(HaveOccurred(), "stderr: %s", stderr)
 
-		By("resolving xxx.neco-ops.cybozu-ne.co")
-		ubuntu := `
-apiVersion: extensions/v1beta1
-kind: Deployment
-metadata:
-  labels:
-    run: test-ubuntu
-  name: test-ubuntu
-  namespace: internet-egress
-spec:
-  selector:
-    matchLabels:
-      run: test-ubuntu
-  template:
-    metadata:
-      labels:
-        run: test-ubuntu
-    spec:
-      containers:
-      - name: test-ubuntu
-        command: ["/bin/sleep", "Infinity"]
-        image: quay.io/cybozu/ubuntu-debug:18.04
-        imagePullPolicy: IfNotPresent
-      securityContext:
-        runAsNonRoot: true
-        runAsUser: 65534 # nobody
-`
-		_, stderr, err = test.ExecAtWithInput(test.Boot0, []byte(ubuntu), "kubectl", "apply", "-f", "-")
-		Expect(err).NotTo(HaveOccurred(), "stderr: %s", stderr)
-
+		By("resolving xxx.neco-ops.dev-ne.co")
 		Eventually(func() error {
-			stdout, _, err := test.ExecAt(test.Boot0, "kubectl", "-n", "internet-egress", "get", "deployments/test-ubuntu", "-o", "json")
-			if err != nil {
-				return err
-			}
-
-			deployment := new(appsv1.Deployment)
-			err = json.Unmarshal(stdout, deployment)
-			if err != nil {
-				return err
-			}
-
-			if deployment.Status.ReadyReplicas != 1 {
-				return errors.New("ReadyReplicas is not 1")
-			}
-			return nil
-		}).Should(Succeed())
-
-		Eventually(func() error {
-			domainName := test.TestID + ".neco-ops.cybozu-ne.co"
-			stdout, _, err := test.ExecAt(test.Boot0, "kubectl", "-n", "internet-egress", "get", "pod", "--selector=run=test-ubuntu", "-o", "json")
-			if err != nil {
-				return err
-			}
-			podList := new(corev1.PodList)
-			err = json.Unmarshal(stdout, podList)
-			if err != nil {
-				return err
-			}
-			if len(podList.Items) != 1 {
-				return errors.New("ubuntu pod doesn't exist")
-			}
-			podName := podList.Items[0].Name
-			stdout, stderr, err := test.ExecAt(test.Boot0, "kubectl", "-n", "internet-egress", "exec", podName, "--", "dig", "+noall", "+answer", "@ns-gcp-private.googledomains.com.", domainName)
+			stdout, stderr, err := test.ExecAt(test.Boot0, "kubectl", "get", "nodes", "-o", "json")
 			if err != nil {
 				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
 			}
-			// expected: xxx.neco-ops.cybozu-ne.co. 300 IN A 10.10.10.10
+
+			var nl corev1.NodeList
+			err = json.Unmarshal(stdout, &nl)
+			if err != nil {
+				return err
+			}
+			if len(nl.Items) == 0 {
+				return errors.New("nodes not found")
+			}
+			node := nl.Items[0]
+
+			var nodeAddress string
+			for _, addr := range node.Status.Addresses {
+				if addr.Type != corev1.NodeInternalIP {
+					continue
+				}
+				nodeAddress = addr.Address
+			}
+			if len(nodeAddress) == 0 {
+				return errors.New("can not get NodeIP")
+			}
+
+			stdout, stderr, err = test.ExecAt(test.Boot0, "ckecli", "ssh", nodeAddress, "dig", "+noall", "+answer", domainName)
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			// expected: xxx.neco-ops.dev-ne.co. 300 IN A 10.0.5.9
 			fields := strings.Fields(string(bytes.TrimSpace(stdout)))
 			if len(fields) < 5 || fields[4] != "10.0.5.9" {
 				return errors.New("expected IP address is 10.0.5.9, but actual response is " + string(stdout))
 			}
 			return nil
 		}).Should(Succeed())
-
-		_, stderr, err = test.ExecAt(test.Boot0, "kubectl", "-n", "internet-egress", "delete", "deployment", "test-ubuntu")
-		Expect(err).NotTo(HaveOccurred(), "stderr: %s", stderr)
 	})
 }
