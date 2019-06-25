1f23ca2673686880237522b38c1b4a50020ff10e
Kazuhito MATSUDA <kazuto.jinnai@gmail.com>
2019-06-25 10:52:44 +0900

    [network-policy] fix test

--- test/network-policy_test.go
+++ test/network-policy_test.go
@@ -4,12 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/cybozu-go/sabakan/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/sparrc/go-ping"
 	"golang.org/x/crypto/ssh"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -131,18 +129,12 @@ spec:
 
 		By("checking ping is dropped")
 		for _, pod := range podList.Items {
-			By(fmt.Sprintf("sending ping to pod: %s[%s]", pod.GetName(), pod.Status.PodIP))
-			pinger, err := ping.NewPinger(pod.Status.PodIP)
-			if err != nil {
-				Expect(err).NotTo(HaveOccurred())
+			if pod.Spec.HostNetwork {
+				continue
 			}
-
-			pinger.Timeout = 2 * time.Second
-			pinger.Count = 1
-			pinger.SetPrivileged(true)
-			pinger.Run()
-			stats := pinger.Statistics()
-			Expect(stats.PacketsRecv).To(Equal(0))
+			By(fmt.Sprintf("  -> ping to %s[%s]", pod.GetName(), pod.Status.PodIP))
+			stdout, stderr, err := ExecAt(boot0, "ping", "-c", "1", "-W", "2", pod.Status.PodIP)
+			Expect(err).To(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
 		}
 
 		By("checking connection")
@@ -158,15 +150,14 @@ spec:
 			{"argocd", "app.kubernetes.io/name=argocd-repo-server", []int{8081, 8084}},
 			{"argocd", "app.kubernetes.io/name=argocd-server", []int{8080, 8083}},
 			{"external-dns", "app=cert-manager", []int{9402}},
+			{"external-dns", "app=webhook", []int{6443}},
 			{"external-dns", "app.kubernetes.io/name=external-dns", []int{7979}},
 			{"ingress", "app=contour", []int{8002, 8080, 8443}},
-			{"internet-egress", "k8s-app=squid", []int{53, 3128}},
+			{"internet-egress", "k8s-app=squid", []int{3128}},
 			{"internet-egress", "k8s-app=unbound", []int{53}},
 			{"kube-system", "cke.cybozu.com/appname=cluster-dns", []int{1053, 8080}},
 			{"kube-system", "k8s-app=kube-state-metrics", []int{8080, 8081}},
-			{"kube-system", "k8s-app=coil-node", []int{9383}},
 			{"metallb-system", "component=controller", []int{7472}},
-			{"metallb-system", "component=speaker", []int{7472}},
 			{"monitoring", "app=alertmanager", []int{9093}},
 			{"monitoring", "app=prometheus", []int{9090}},
 		}
@@ -183,12 +174,18 @@ spec:
 			for _, pod := range podList.Items {
 				By(fmt.Sprintf("checking pod: %s[%s]", pod.GetName(), pod.Status.PodIP))
 				for _, port := range tc.ports {
-					By(fmt.Sprintf("dialing to allowed port: %d", port))
+					By(fmt.Sprintf("  -> allowed port: %d", port))
 					stdout, stderr, err = ExecAtWithInput(boot0, []byte("Xclose"), "timeout", "3s", "telnet", pod.Status.PodIP, strconv.Itoa(port), "-e", "X")
-					Expect(err).NotTo(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
+					switch t := err.(type) {
+					case *ssh.ExitError:
+						// telnet command returns 124 when it times out
+						Expect(t.ExitStatus()).NotTo(Equal(124))
+					default:
+						Expect(err).NotTo(HaveOccurred())
+					}
 				}
 
-				By(fmt.Sprintf("dialing to denied port: %d", portShouldBeDenied))
+				By(fmt.Sprintf("  -> denied port: %d", portShouldBeDenied))
 				stdout, stderr, err = ExecAtWithInput(boot0, []byte("Xclose"), "timeout", "3s", "telnet", pod.Status.PodIP, strconv.Itoa(portShouldBeDenied), "-e", "X")
 				switch t := err.(type) {
 				case *ssh.ExitError:
@@ -215,7 +212,7 @@ spec:
 		err = json.Unmarshal(stdout, &machines)
 		Expect(err).ShouldNot(HaveOccurred())
 		for _, m := range machines {
-			By("sending: " + m.Spec.BMC.IPv4)
+			By("  -> ping to " + m.Spec.BMC.IPv4)
 			stdout, _, err := ExecAt(boot0, "kubectl", "exec", "ubuntu", "--", "ping", "-W", "3", "-c", "1", m.Spec.BMC.IPv4)
 			Expect(err).To(HaveOccurred(), "stdout: %s", stdout)
 		}
