780ce6acca55e98ecf46f837eb95aeeb93894c6e
Kazuhito MATSUDA <kazuto.jinnai@gmail.com>
2019-07-23 13:17:00 +0900

    fix

--- test/teleport_test.go
+++ test/teleport_test.go
@@ -29,7 +29,7 @@ func testTeleport() {
 
 		By("storing LoadBalancer IP address to etcd")
 		ExecSafeAt(boot0, "env", "ETCDCTL_API=3", "etcdctl", "--cert=/etc/etcd/backup.crt", "--key=/etc/etcd/backup.key",
-			"put", "/neco/teleport/auth-servers", `["`+addr+`:3025"]`)
+			"put", "/neco/teleport/auth-servers", `[\"`+addr+`:3025\"]`)
 
 		By("starting teleport node services on boot servers")
 		for _, h := range []string{boot0, boot1, boot2} {
