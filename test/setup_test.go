24460e0ba07af9a15e60d657750ba80162a75d4b
mitz <mitz@cybozu.co.jp>
2019-09-20 11:09:42 +0900

    Fix creating dummy license secret

--- test/setup_test.go
+++ test/setup_test.go
@@ -226,9 +226,6 @@ func testSetup() {
 				ExecSafeAt(boot0, "kubectl", "-n", "teleport", "create", "secret", "generic",
 					"teleport-etcd-certs", "--from-file=ca.crt=etcd-ca.crt",
 					"--from-file=tls.crt=etcd-teleport.crt", "--from-file=tls.key=etcd-teleport.key")
-
-				stdout, stderr, err = ExecAtWithInput(boot0, []byte(teleportEnterpriseLicenseSecret), "kubectl", "create", "-f", "-")
-				Expect(err).NotTo(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
 			}
 
 			By("creating namespace and secrets for elastic")
@@ -245,6 +242,14 @@ func testSetup() {
 		Expect(err).NotTo(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
 		stdout, stderr, err = ExecAtWithInput(boot0, []byte(gatekeeperSecret), "kubectl", "apply", "-f", "-")
 		Expect(err).NotTo(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
+	})
+
+	It("should prepare secrets for teleport", func() {
+		//TODO: move into `if !doUpgrade {}` when the teleport enterprise is deployed
+		if !withKind {
+			stdout, stderr, err := ExecAtWithInput(boot0, []byte(teleportEnterpriseLicenseSecret), "kubectl", "create", "-f", "-")
+			Expect(err).NotTo(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
+		}
 	})
 
 	It("should checkout neco-apps repository@"+commitID, func() {
