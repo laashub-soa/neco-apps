0ef19fb8d431594b2836b23066df29df1bdac366
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2019-09-27 10:50:15 +0000

    Fix quoting in ExecSafeAt().

--- test/setup_test.go
+++ test/setup_test.go
@@ -302,7 +302,7 @@ func testSetup() {
 			ExecSafeAt(boot0, "neco", "config", "set", "proxy", proxyURL)
 			ExecSafeAt(boot0, "neco", "config", "set", "node-proxy", proxyURL)
 
-			necoVersion := string(ExecSafeAt(boot0, "dpkg-query", "-W", "-f", "${Version}", "neco"))
+			necoVersion := string(ExecSafeAt(boot0, "dpkg-query", "-W", "-f", "'${Version}'", "neco"))
 			rolePaths := strings.Split(string(ExecSafeAt(boot0, "ls", "-1", "/usr/share/neco/ignitions/roles/*/site.yml")), "\n")
 			for _, rolePath := range rolePaths {
 				role := strings.Split(rolePath, "/")[6]
