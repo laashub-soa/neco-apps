c3f307a1e4ac753e2a7e9c7c6add907b047831ed
Hiroshi Muraoka <hiroshi-muraoka@cybozu.co.jp>
2019-10-01 02:47:30 +0000

    Improve gatekeeoer test to wait for ready

--- test/gatekeeper_test.go
+++ test/gatekeeper_test.go
@@ -7,9 +7,82 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func testGatekeeper() {
+	It("should make gatekeeper ready", func() {
+		By("confirming that controller manager pod is ready")
+		Eventually(func() error {
+			stdout, stderr, err := ExecAt(boot0, "kubectl", "get", "-n=gatekeeper-system", "pods", "gatekeeper-controller-manager-0", "-o", "json")
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+
+			var pod corev1.Pod
+			err = json.Unmarshal(stdout, &pod)
+			if err != nil {
+				return err
+			}
+
+			if len(pod.Status.Conditions) == 0 {
+				return errors.New("conditions not found")
+			}
+
+			for _, cond := range pod.Status.Conditions {
+				if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+					return nil
+				}
+			}
+			return errors.New("condition 'Ready' is not found")
+		}).Should(Succeed())
+
+		By("confirming that constraint templates is established")
+		Eventually(func() error {
+			stdout, stderr, err := ExecAt(boot0, "kubectl", "get", "crd", "constrainttemplates.templates.gatekeeper.sh", "-o", "json")
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+
+			var crd struct {
+				Status struct {
+					Conditions []struct {
+						Type   string `json:"type"`
+						Status string `json:"status"`
+					} `json:"conditions"`
+				} `json:"status"`
+			}
+			err = json.Unmarshal(stdout, &crd)
+			if err != nil {
+				return err
+			}
+
+			if len(crd.Status.Conditions) == 0 {
+				return errors.New("conditions not found")
+			}
+
+			for _, cond := range crd.Status.Conditions {
+				if cond.Type == "Established" && cond.Status == "True" {
+					return nil
+				}
+			}
+			return errors.New("condition 'Established' is not found")
+		}).Should(Succeed())
+
+		By("confirming validating webhook can be got")
+		Eventually(func() error {
+			stdout, stderr, err := ExecAt(
+				boot0, "kubectl", "get",
+				"validatingwebhookconfigurations.admissionregistration.k8s.io", "validation.gatekeeper.sh",
+			)
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			return nil
+		}).Should(Succeed())
+
+	})
+
 	It("should validate Service resource according to policy", func() {
 		By("registering constraint template")
 		constraintTemplete := `
@@ -42,12 +115,39 @@ spec:
           msg := sprintf("you must provide labels: %v", [missing])
         }
 `
+		stdout, stderr, err := ExecAtWithInput(boot0, []byte(constraintTemplete), "kubectl", "apply", "-f", "-")
+		Expect(err).To(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
+
+		By("confirming that required labels constraint is established")
 		Eventually(func() error {
-			stdout, stderr, err := ExecAtWithInput(boot0, []byte(constraintTemplete), "kubectl", "apply", "-f", "-")
+			stdout, stderr, err := ExecAt(boot0, "kubectl", "get", "crd", "k8srequiredlabels.constraints.gatekeeper.sh", "-o", "json")
 			if err != nil {
 				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
 			}
-			return nil
+
+			var crd struct {
+				Status struct {
+					Conditions []struct {
+						Type   string `json:"type"`
+						Status string `json:"status"`
+					} `json:"conditions"`
+				} `json:"status"`
+			}
+			err = json.Unmarshal(stdout, &crd)
+			if err != nil {
+				return err
+			}
+
+			if len(crd.Status.Conditions) == 0 {
+				return errors.New("conditions not found")
+			}
+
+			for _, cond := range crd.Status.Conditions {
+				if cond.Type == "Established" && cond.Status == "True" {
+					return nil
+				}
+			}
+			return errors.New("condition 'Established' is not found")
 		}).Should(Succeed())
 
 		By("registering constraint")
@@ -64,13 +164,8 @@ spec:
   parameters:
     labels: ["gatekeeper"]
 `
-		Eventually(func() error {
-			stdout, stderr, err := ExecAtWithInput(boot0, []byte(constraint), "kubectl", "apply", "-f", "-")
-			if err != nil {
-				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
-			}
-			return nil
-		}).Should(Succeed())
+		stdout, stderr, err = ExecAtWithInput(boot0, []byte(constraint), "kubectl", "apply", "-f", "-")
+		Expect(err).To(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
 
 		By("waiting for constrains enforced")
 		Eventually(func() error {
