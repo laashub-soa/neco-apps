5fdd67510fb407b7bc450da6e2ceb66460e9ffb4
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2019-10-01 06:36:29 +0000

    test: fix for golint.

--- test/external-dns_test.go
+++ test/external-dns_test.go
@@ -92,7 +92,7 @@ spec:
 			if len(fields) < 5 || fields[4] != "10.0.5.9" {
 				output := ExecSafeAt(boot0, "kubectl", "get", "-n", "ingress", "dnsendpoint", "test-endpoint", "-o", "yaml")
 				output2 := ExecSafeAt(boot0, "kubectl", "-n", "external-dns", "logs", "-l", "app.kubernetes.io/name=external-dns", "--tail", "10000")
-				return fmt.Errorf("expected IP address is 10.0.5.9, but actual response is %s\ndump of DNSEndpoint: %s\nexternal-dns logs: %s\n", string(stdout), string(output), string(output2))
+				return fmt.Errorf("expected IP address is 10.0.5.9, but actual response is %s\ndump of DNSEndpoint: %s\nexternal-dns logs: %s", string(stdout), string(output), string(output2))
 			}
 			return nil
 		}).Should(Succeed())
