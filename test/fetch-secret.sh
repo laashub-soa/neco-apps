5107b82e8209d3b93ffdf2e330a2c6656dfaeeb7
zoetrope <a.ikezoe@gmail.com>
2019-10-09 14:56:16 +0900

    fix branch name

--- test/fetch-secret.sh
+++ test/fetch-secret.sh
@@ -8,8 +8,14 @@ else
     GIT_URL="https://${GIT_USER}:${SECRET_GITHUB_TOKEN}@github.com/cybozu-private/neco-apps-secret"
 fi
 
+if [ "${CIRCLE_BRANCH}" != "release" -o "${CIRCLE_BRANCH}" != "stage" ]; then
+  BRANCH="master"
+else
+  BRANCH=${CIRCLE_BRANCH}
+fi
+
 rm -rf ./neco-apps-secret
-git clone -b add-secrets $GIT_URL neco-apps-secret 2> /dev/null
+git clone -b $BRANCH $GIT_URL neco-apps-secret 2> /dev/null
 
 kustomize build ./neco-apps-secret/base > expected-secret.yaml
 kustomize build ../secrets/base > current-secret.yaml
