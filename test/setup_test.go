c1997c0a349fe47ea5d4cec9f5860d23424cd9f8
binoue <banji-inoue@cybozu.co.jp>
2020-01-20 10:04:43 +0900

    Add codes to remove resource quota

--- test/setup_test.go
+++ test/setup_test.go
@@ -295,6 +295,18 @@ func applyAndWaitForApplications(overlay string) {
 	fmt.Printf("application list: %v\n", appList)
 	Expect(appList).ShouldNot(HaveLen(0))
 
+	By("pruning resource quota manually")
+	//TODO: Remove this `By` block after merged this PR: https://github.com/cybozu-go/neco-apps/pull/344
+	if doUpgrade {
+		Eventually(func() error {
+			stdout, stderr, err := ExecAt(boot0, "argocd", "app", "sync", "team-management", "--prune")
+			if err != nil {
+				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			return nil
+		}).Should(Succeed())
+	}
+
 	By("waiting initialization")
 	Eventually(func() error {
 		for _, appName := range appList {
