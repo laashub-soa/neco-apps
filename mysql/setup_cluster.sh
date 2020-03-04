7ad91a37e7fa2af877c5bdc59ede323797a2e827
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2020-03-04 09:38:30 +0000

    Fix mysql manifest
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- mysql/setup_cluster.sh
+++ mysql/setup_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mysqlsh --no-wizard --py --uri root:${MYSQL_ROOT_PASSWORD}@my-app-db-0.my-app-db:3306 -e "dba.create_cluster('Cluster', {'ipWhitelist': '10.0.0.0/8'})"
-mysqlsh --no-wizard --py --uri root:${MYSQL_ROOT_PASSWORD}@my-app-db-0.my-app-db:3306 -e "dba.get_cluster('Cluster').add_instance('root:${MYSQL_ROOT_PASSWORD}@my-app-db-1.my-app-db:3306', {'ipWhitelist': '10.0.0.0/8'})"
-mysqlsh --no-wizard --py --uri root:${MYSQL_ROOT_PASSWORD}@my-app-db-0.my-app-db:3306 -e "dba.get_cluster('Cluster').add_instance('root:${MYSQL_ROOT_PASSWORD}@my-app-db-2.my-app-db:3306', {'ipWhitelist': '10.0.0.0/8'})"
+mysqlsh --no-wizard --py --uri root:${MYSQL_ROOT_PASSWORD}@my-app-db-0.my-app-db:3306 -e "dba.get_cluster('Cluster').add_instance('root:${MYSQL_ROOT_PASSWORD}@my-app-db-1.my-app-db:3306', {'ipWhitelist': '10.0.0.0/8', 'recoveryMethod': 'clone'})"
+mysqlsh --no-wizard --py --uri root:${MYSQL_ROOT_PASSWORD}@my-app-db-0.my-app-db:3306 -e "dba.get_cluster('Cluster').add_instance('root:${MYSQL_ROOT_PASSWORD}@my-app-db-2.my-app-db:3306', {'ipWhitelist': '10.0.0.0/8', 'recoveryMethod': 'clone'})"
