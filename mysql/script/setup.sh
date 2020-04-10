493f7ec41c9c35cfcfcf93500095377e61644039
dulltz <isrgnoe@gmail.com>
2020-04-10 12:10:45 +0900

    update image to quay.io/tsuruda/mysql
    add copy of my-app-db
    
    Signed-off-by: dulltz <isrgnoe@gmail.com>

--- mysql/script/setup.sh
+++ mysql/script/setup.sh
@@ -2,7 +2,7 @@
 
 function setup_master() {
     TARGET_POD=$1
-    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.${MYSQL_CLUSTER_DOMAIN}:3306 << EOS
+    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.my-app-db-copy:3306 << EOS
 SET GLOBAL rpl_semi_sync_slave_enabled = 0;
 SET GLOBAL rpl_semi_sync_master_enabled = 1;
 SET GLOBAL offline_mode = 0;
@@ -12,7 +12,7 @@ EOS
 
 function setup_slave() {
     TARGET_POD=$1
-    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.${MYSQL_CLUSTER_DOMAIN}:3306 << EOS
+    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.my-app-db-copy:3306 << EOS
 SET GLOBAL rpl_semi_sync_slave_enabled = 1;
 SET GLOBAL rpl_semi_sync_master_enabled = 0;
 SET GLOBAL offline_mode = 1;
@@ -23,9 +23,9 @@ EOS
 function change_master_to() {
     TARGET_POD=$1
     master=$2
-    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.${MYSQL_CLUSTER_DOMAIN}:3306 << EOS
+    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.my-app-db-copy:3306 << EOS
 CHANGE MASTER TO
-  MASTER_HOST = '${master}.${MYSQL_CLUSTER_DOMAIN}',
+  MASTER_HOST = '${master}.my-app-db-copy',
   MASTER_PORT = 3306,
   MASTER_USER = '${MYSQL_REPLICATION_USER}',
   MASTER_PASSWORD = '${MYSQL_REPLICATION_PASSWORD}',
@@ -37,14 +37,14 @@ EOS
 
 function disable_read_only() {
     TARGET_POD=$1
-    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.${MYSQL_CLUSTER_DOMAIN}:3306 << EOS
+    mysqlsh --no-wizard --sql --uri ${MYSQL_OPERATOR_USER}:${MYSQL_OPERATOR_PASSWORD}@${TARGET_POD}.my-app-db-copy:3306 << EOS
 SET GLOBAL read_only = 0;
 EOS
 }
 
-setup_master my-app-db-0
-setup_slave my-app-db-1
-setup_slave my-app-db-2
-change_master_to my-app-db-1 my-app-db-0
-change_master_to my-app-db-2 my-app-db-0
-disable_read_only my-app-db-0
+setup_master my-app-db-copy-0
+setup_slave my-app-db-copy-1
+setup_slave my-app-db-copy-2
+change_master_to my-app-db-copy-1 my-app-db-copy-0
+change_master_to my-app-db-copy-2 my-app-db-copy-0
+disable_read_only my-app-db-copy-0
